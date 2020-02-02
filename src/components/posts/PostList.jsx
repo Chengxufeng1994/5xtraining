c35c94e43abc98675af312adabe27a7074490957
xufengcheng <benny12628@gmail.com>
2020-02-03 00:05:21 +0800

    新增prettier

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 import PostItems from './PostItems';
 import Pagination from './Pagination';
